29f710e40f6fd9a3f9681290e994c4b02cd47c0f
hmohideen <hmohideen@vmware.com>
2022-10-01 19:29:15 +0530

    Added desc. with examples

--- src/com/dp/factory/Demo.java
+++ src/com/dp/factory/Demo.java
@@ -1,6 +1,23 @@
 package com.dp.factory;
 
+import java.util.Calendar;
+
 public class Demo {
+	
+	/*
+	 * Factory Pattern (Creational Pattern):
+	 * 1. Used for creation of Object that falls under same category but still with
+	 * 		different properties.
+	 * 2. Hides Complexity to user.
+	 * 
+	 * Pros:
+	 * 1. Single Responsibility principle --> Product creation code can be kepy at one place making
+	 * 		it easy to support.
+	 * 2. Open/ Closed Principle --> New products can be added on need without breaking existing code.
+	 * 
+	 * Cons:
+	 * 1. Comples coding as it cannot be refactored and takes time for set the base.
+	 */
 
 	public static void main(String[] args) {
 		Course hldCourses = CourseFactory.getCourse("HLD");
@@ -13,4 +30,12 @@ public class Demo {
 		System.out.println(lldCourses.modules);
 	}
 
+	/*
+	 * Examples:
+	 * 1. Calendar.getInstance()
+	 * 2. NumberFormat.getInstance()
+	 */
+	public void example() {
+		System.out.println(Calendar.getInstance());
+	}
 }
